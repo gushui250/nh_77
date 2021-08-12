8f301b5a61ae4cf694bdd33c8c401b5021edd91a
gushui250 <2770488802@qq.com>
2021-08-12 14:25:15 +0800

    优化增加api-检测是否有效按钮

--- api_test/check_api.py
+++ api_test/check_api.py
@@ -9,17 +9,25 @@ import time
 import requests
 import xlrd
 
-with xlrd.open_workbook(fr'E:\nh修正版\nh\data\接口文档.xlsx') as t:
-    y = len(t.sheets())
-    if 1 <= y:
-        sheet = t.sheets()[1 - 1]
-        for i in range(sheet.nrows):
-            for i in sheet.row_values(i):
-                url = i
-                try:
-                    res = requests.get(url)
-                    html_1 = res.status_code
-                    if html_1 != 200:
-                        time.sleep(0.2)
-                except EnvironmentError as  e:
-                    print(f'该链接：{url}，无法访问，错误原因： {e} ')
+
+class Api():
+    def check_api_false(self, file):
+        # 检测api是否有效
+        with xlrd.open_workbook(file) as t:
+            y = len(t.sheets())
+            if 1 <= y:
+                sheet = t.sheets()[1 - 1]
+                for i in range(sheet.nrows):
+                    for i in sheet.row_values(i):
+                        url = i
+                        try:
+                            res = requests.get(url)
+                            html_1 = res.status_code
+                            if html_1 != 200:
+                                time.sleep(0.2)
+                        except EnvironmentError as  e:
+                            print(f'该链接：{url}，无法访问，错误原因： {e} ')
+
+
+if __name__ == "__main__":
+    Api().check_api_false(r'E:\nh修正版\nh\data\接口文档.xlsx')
